a5f6311bf8a2862bc507b39b026f05c54995f246
Ria vasiliu <elefioni@gmail.com>
2020-12-06 21:54:57 +0100

    chaos

--- topdown-ransac-example.R
+++ topdown-ransac-example.R
@@ -4,8 +4,14 @@ data_simple <- make_ransac_data(n_obs = 100, n_coef = 1, inlier_fraction = 0.7)
 
 # univariate example:
 ransac_simple <- ransaclm(y ~ . - inlier,
+                                   data = data_simple, error_threshold = 2,
+                                   inlier_threshold = 50, seed = 20171111
+)
+ransac_simple_parallel <- ransaclm_parallel(y ~ . - inlier,
                           data = data_simple, error_threshold = 2,
-                          inlier_threshold = 50, seed = 20171111
+                          inlier_threshold = 50, seed = 20171111,
+                          workers = 2
 )
 validate_ransac(ransac_simple)
+validate_ransac(ransac_simple_parallel)
 
